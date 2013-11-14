affc95f74aacf9131cfe8a9eb43559372f18e110
Nick Bennett <nick@ohrho.com>
2013-11-14 04:58:35 +0000

    First post.

---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Nick Bennett'
+SITENAME = u'ohrho'
+SITEURL = 'http://ohrho.com'
+
+TIMEZONE = 'America/Chicago'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('Hacker News', 'http://news.ycombinator.com/'),
+        ('lobste.rs', 'http://lobste.rs/'),)
+
+# Social widget
+SOCIAL = (('my linkedin', 'https://linkedin.com/in/nicholasrrbennett/'),
+          ('my twitter', 'https://twitter.com/nicksdomainhack'),
+          ('my github', 'https://github.com/tothebeat'),)
+
+DEFAULT_PAGINATION = 8
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
